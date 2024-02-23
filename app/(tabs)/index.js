01a213301d552e3d5f67bd994e2e85d47f5b9c5e
Ralf Siewert <ralf@actyvy.st>
2024-02-23 11:16:38 +0100

    add typography component

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -2,18 +2,28 @@ import { Text, ScrollView, Button } from "react-native";
 import { Link } from "expo-router";
 import { globalStyles } from "../../styles/global";
 import { COLORS } from "../../styles/constants";
+import { Typography } from "../../components/Typography";
 
 export default function HomePage() {
   return (
     <ScrollView style={globalStyles.container}>
-      <Text style={[globalStyles.h1]}>World of movies</Text>
-      <Text style={globalStyles.p}>
+      <Typography
+        onPress={() => {
+          alert("Typography");
+        }}
+        variant="heading"
+        style={{ color: "green" }}
+      >
+        World of movies
+      </Typography>
+      <Typography>
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
         clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
         amet.
-      </Text>
+      </Typography>
+
       <Link asChild style={globalStyles.link} href="movies">
         <Button color={COLORS.accent} title="Movies" />
       </Link>
